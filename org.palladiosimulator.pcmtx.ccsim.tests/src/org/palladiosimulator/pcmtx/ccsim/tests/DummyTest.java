f3929995a7b44c4d781a8392d87a8185a73644b1
pmerkle <merkle@kit.edu>
2016-05-19 08:39:31 +0000

    add dummy test required by buckminster
    git-svn-id: https://svnserver.informatik.kit.edu/i43/svn/code/Palladio/Addons/Palladio.TX/trunk@32038 769cc9a0-fb0c-0410-8c2f-a2305f0e6268

---
+++ org.palladiosimulator.pcmtx.ccsim.tests/src/org/palladiosimulator/pcmtx/ccsim/tests/DummyTest.java
@@ -0,0 +1,13 @@
+package org.palladiosimulator.pcmtx.ccsim.tests;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class DummyTest {
+
+	@Test
+	public void testDummy() {
+		Assert.assertEquals(true, true);
+	}
+	
+}
