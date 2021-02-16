f29b10093156ba388973e8c671e66051692aac22
Stephan Seifermann <stephan.seifermann@kit.edu>
2021-02-16 17:51:43 +0100

    PALLADIO-530 Use PCM Stoex API

--- bundles/edu.kit.ipd.sdq.randomutils/src/edu/kit/ipd/sdq/randomutils/ManagedPDFParser.java
+++ bundles/edu.kit.ipd.sdq.randomutils/src/edu/kit/ipd/sdq/randomutils/ManagedPDFParser.java
@@ -1,30 +1,32 @@
 package edu.kit.ipd.sdq.randomutils;
 
-import org.palladiosimulator.commons.stoex.api.StoExParser.SyntaxErrorException;
+import org.palladiosimulator.pcm.stoex.api.StoExParser;
+import org.palladiosimulator.pcm.stoex.api.StoExParser.SyntaxErrorException;
 
 import de.uka.ipd.sdq.probfunction.ProbabilityDensityFunction;
 import de.uka.ipd.sdq.probfunction.ProbabilityMassFunction;
 import de.uka.ipd.sdq.probfunction.math.ManagedPDF;
 import de.uka.ipd.sdq.stoex.Expression;
 import de.uka.ipd.sdq.stoex.ProbabilityFunctionLiteral;
-import edu.kit.ipd.sdq.randomutils.internal.Activator;
 
 public final class ManagedPDFParser {
+
+    protected static final StoExParser STOEX_PARSER = StoExParser.createInstance();
 
     private ManagedPDFParser() {
         // intentionally left blank
     }
-    
+
     public static ManagedPDF createFromString(String pdfAsString) throws StringNotPDFException {
         ProbabilityFunctionLiteral value = parse(pdfAsString);
-        
+
         if (!(value.getFunction_ProbabilityFunctionLiteral() instanceof ProbabilityMassFunction)) {
             throw new StringNotPDFException();
         }
         ProbabilityDensityFunction pdf = (ProbabilityDensityFunction) value.getFunction_ProbabilityFunctionLiteral();
         return new ManagedPDF(pdf);
     }
-    
+
     private static ProbabilityFunctionLiteral parse(String s) throws StringNotPDFException {
         try {
             int iterInt = Integer.parseInt(s);
@@ -34,17 +36,15 @@ public final class ManagedPDFParser {
 
         Expression parsedStoEx;
         try {
-            parsedStoEx = Activator.getInstance()
-                .getStoexParser()
-                .parse(s);
+            parsedStoEx = STOEX_PARSER.parse(s);
         } catch (SyntaxErrorException e) {
             throw new StringNotPDFException(e.getMessage());
         }
-        
+
         if (!(parsedStoEx instanceof ProbabilityFunctionLiteral)) {
             throw new StringNotPDFException();
         }
         return (ProbabilityFunctionLiteral) parsedStoEx;
     }
-    
+
 }
